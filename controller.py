6a6559cb46bd766e581d986a82f284ac3c6414b4
Albert Lee <awlee@princeton.edu>
2015-05-13 22:20:37 -0400

    Clean and comment

--- controller.py
+++ controller.py
@@ -7,7 +7,6 @@ import controller
 import model
 import head
 from collections import namedtuple
-#from theano_toolkit.parameters import Parameters
 
 def build(P,input_size,output_size,mem_size,mem_width,layer_size):
 	"""
@@ -27,15 +26,17 @@ def build(P,input_size,output_size,mem_size,mem_width,layer_size):
 	P.b_output = 0. * U.initial_weights(output_size)
 
 	def controller(input_t,read_t):
-		prev_layer = hidden_0 = T.tanh(
-				T.dot(input_t,P.W_input_hidden) +\
-				T.dot(read_t,P.W_read_hidden) +\
-				P.b_hidden_0
-			)
+                """
+                Controller consists of single hidden layer between inputs and outputs
+                """
+		hidden_layer = T.tanh(
+                        T.dot(input_t,P.W_input_hidden) +\
+                        T.dot(read_t,P.W_read_hidden) +\
+                        P.b_hidden_0
+                )
 
-		fin_hidden = prev_layer
-		output_t = T.nnet.sigmoid(T.dot(fin_hidden,P.W_hidden_output) + P.b_output)
+		output_t = T.nnet.sigmoid(T.dot(hidden_layer,P.W_hidden_output) + P.b_output)
 
-                # Return output and final hidden layer of controller (passed to heads)
-		return output_t,fin_hidden
+                # Return output and hidden layer of controller used by heads (in model.py)
+		return output_t,hidden_layer
 	return controller
